7f062b5d4ad6cb02042e1d4d73d29b57278c1c64
IgorAlvesSG <igorasgoes@gmail.com>
2026-04-14 01:30:56 -0300

    Commit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function trocarPagina(idAlvo){
-    const secoes = decodeURIComponent.querySelectorAll('.secao-conteudo');
+function trocarPagina(idAlvo) {
+    // 1. Pega todas as seções da página
+    const secoes = document.querySelectorAll('.secao-conteudo'); // Corrigido para 'document'
+    
+    // 2. Esconde todas elas
     secoes.forEach(secao => {
         secao.style.display = 'none';
-
-    })
-};
-const alvo = document.getElementById(idAlvo);
-if (alvo) {
-    alvo.style.display = 'flex'; 
-    alvo.style.flexDirection = 'column';
-
+    });
+    
+    // 3. Mostra apenas a que você clicou (Tudo isso AGORA ESTÁ DENTRO DA FUNÇÃO)
+    const alvo = document.getElementById(idAlvo);
+    if (alvo) {
+        alvo.style.display = 'flex'; 
+        alvo.style.flexDirection = 'column';
+    }
 }
